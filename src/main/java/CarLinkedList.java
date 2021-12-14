25dd9d6bae511dddf1a2756dfbdd377b96ffd40b
Roman Peshaev <romka3842@gmail.com>
2021-12-14 21:34:30 +0300

    Add CarLinkedList

---
+++ src/main/java/CarLinkedList.java
@@ -0,0 +1,128 @@
+public class CarLinkedList implements CarList {
+
+    private Node first;
+    private Node last;
+    private int size = 0;
+
+    @Override
+    public Car get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(Car car) {
+        if(size == 0)
+        {
+            first = new Node(null,car,null);
+            last = first;
+        }
+        else
+        {
+            Node secondLast = last;
+            last = new Node(secondLast,car,null);
+            secondLast.next=last;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        Node node = first;
+        for(int i =0;i<size;i++)
+        {
+            if(node.value.equals(car))
+            {
+                removeAt(i);
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+
+        Node node = getNode(index);
+        Node nodeNext = node.next;
+        Node nodePrevious =  node.previous;
+        if(nodeNext != null) {
+            nodeNext.previous = nodePrevious;
+        }
+        else
+        {
+            last = nodePrevious;
+        }
+        if(nodePrevious !=null) {
+            nodePrevious.next = nodeNext;
+        }
+        else
+        {
+            first = nodeNext;
+        }
+        size--;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(Car car, int index) {
+        checkIndex(index);
+        if(index==size)
+        {
+            add(car);
+            return;
+        }
+        Node nodeNext = getNode(index);
+        Node nodePrevious = nodeNext.previous;
+        Node newNode = new Node(nodePrevious,car,nodeNext);
+        nodeNext.previous = newNode;
+        if(nodePrevious !=null)
+        {
+            nodePrevious.next = newNode;
+        }
+        else
+        {
+            first = newNode;
+        }
+        size++;
+    }
+
+    private void checkIndex(int index)
+    {
+        if(index < 0 || index>size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node getNode(int index)
+    {
+        Node node = first;
+        for(int i = 0; i <index; i++)
+        {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private static class Node
+    {
+        private Node previous;
+        private Car value;
+        private Node next;
+
+        public Node(Node previous, Car value, Node next) {
+            this.previous = previous;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
